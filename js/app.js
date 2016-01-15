4e44f4afd74948c58c464e6a729a2004e44c9048
marioosh <marioosh.net@gmail.com>
2016-01-15 11:53:47 +0100

    simple routing + partial views

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 'use strict';
 
-angular.module('app',['mainModule'])
+angular.module('app',[
+	'ngRoute',
+	'mainModule'
+])
+.config(['$routeProvider', function($routeProvider) {
+  // when no route match found 
+  $routeProvider.otherwise({redirectTo: '/hello'});
+}])
 .run(function($rootScope){
 	// when all modules loaded
 	$rootScope.title = 'Angular Starter';
--- js/modules/main/js/controllers.js
+++ js/modules/main/js/controllers.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('app.main.controllers', [])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/hello', {
+    templateUrl: 'js/modules/main/views/hello.html',
+    controller: 'HelloController'
+  })
+  .when('/welcome', {
+    templateUrl: 'js/modules/main/views/welcome.html',
+    controller: 'WelcomeController'
+  });
+}])
 .controller('HelloController', function($scope) {
   $scope.hello = 'Hello World!';
+})
+.controller('WelcomeController', function($scope) {
+  $scope.welcome = 'Welcome!!';
 });
