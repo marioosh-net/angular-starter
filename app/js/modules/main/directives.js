af0ce4475fc77e23f2665f42a41f0b2371d4c48a
marioosh <marioosh.net@gmail.com>
2016-01-22 12:31:48 +0100

    directive with isolated scope

--- app/js/modules/main/directives.js
+++ app/js/modules/main/directives.js
@@ -21,11 +21,39 @@ angular.module('app.main.directives', ['restModule'])
 		restrict: 'AEC', // directive can be used as an attribute, a HTML element or class.
 		replace: true, // generated template will replace the HTML element on which the directive is applied
 		templateUrl: 'views/weather-info.html',
-		controller: function ($scope, $element, $attrs, $transclude) {
+		/* controller: function ($scope, $element, $attrs, $transclude) {
 			WeatherService.currentWeather($attrs.city)
 		    .success(function(data,status,config,headers){
 		    	$scope.weather = data;
     		});			
-		}
+		},*/
+        link: function(scope, elem, attrs) {
+            // scope - by default, a directive gets its parent's scope
+			WeatherService.currentWeather(attrs.city)
+		    .success(function(data,status,config,headers){
+		    	scope.weather = data;
+    		});			        	
+        }
+	};
+}])
+
+
+/**
+ * current-weather
+ * with isolated scope
+ */
+.directive('currentWeatherSafe', ['WeatherService', function(WeatherService){
+	return {
+		//scope: true, // use a child scope that inherits from parent
+		scope: {}, // use a new isolated scope 
+		restrict: 'AEC', // directive can be used as an attribute, a HTML element or class.
+		replace: true, // generated template will replace the HTML element on which the directive is applied
+		templateUrl: 'views/weather-info.html',
+        link: function(scope, elem, attrs) {
+			WeatherService.currentWeather(attrs.city)
+		    .success(function(data,status,config,headers){
+		    	scope.weather = data;
+    		});			        	
+        }
 	};
 }]);
