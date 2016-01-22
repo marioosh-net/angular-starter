e111faa031919a4bf9fcf95c83e2723099c686bf
marioosh <marioosh.net@gmail.com>
2016-01-22 19:48:16 +0100

    directive: using '=' for 2-way binding into isolated scope

--- app/js/modules/maps/directives.js
+++ app/js/modules/maps/directives.js
@@ -2,25 +2,27 @@
 
 angular.module('app.maps.directives', [])
 
-.directive('googleMaps', ['GOOGLE_MAPS_API_KEY', '$sce', function(GOOGLE_MAPS_API_KEY, $sce){
+.directive('googleMaps', ['GOOGLE_MAPS_API_KEY', 'GOOGLE_MAPS_API_URL', '$sce', function(GOOGLE_MAPS_API_KEY, GOOGLE_MAPS_API_URL, $sce){
 	return {
 		scope: {
-    		height: '@height',
+    		height: '@height', // @ or @attr - bind a local scope property to the value of DOM attribute.
         	width: '@width',
-        	lat: '@lat',
-        	lon: '@lon'
+        	coord: '=' // = or =attr - set up bi-directional binding between a local scope property and the parent scope property of name defined via the value of the attr attribute
 		},
 		restrict: 'AEC', // directive can be used as an attribute, a HTML element or class.
 		replace: true, // generated template will replace the HTML element on which the directive is applied
 		templateUrl: 'views/google-maps.html',
         link: function(scope, elem, attrs) {
-        	var f = function(value){
-        		console.log(value);
-        		scope.src = $sce.trustAsResourceUrl('https://www.google.com/maps/embed/v1/view?center='+scope.lat+','+scope.lon+'&zoom=10&key='+GOOGLE_MAPS_API_KEY);
-        	};
-        	scope.$watch('lat', f);
-        	scope.$watch('lon', f);        	
+        	scope.$watch('coord', function(value,old){
+                if(value!=old) {
+                    if(scope.coord!=null) {
+                        //elem.attr('src', GOOGLE_MAPS_API_URL+'/view?center='+scope.coord.lat+','+scope.coord.lon+'&zoom=10&key='+GOOGLE_MAPS_API_KEY);
+                        scope.src = $sce.trustAsResourceUrl(GOOGLE_MAPS_API_URL+'/view?center='+scope.coord.lat+','+scope.coord.lon+'&zoom=10&key='+GOOGLE_MAPS_API_KEY);;
+                    }
+                }
+            });
         }
 	};	
 }])
+.value('GOOGLE_MAPS_API_URL', 'https://www.google.com/maps/embed/v1')
 .value('GOOGLE_MAPS_API_KEY', 'AIzaSyC1WrNUv4iF_ay6EiohLYa34KnXz1mMIB8');
