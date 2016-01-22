bf9d05f6878650312492638a82f631f8321f8023
marioosh <marioosh.net@gmail.com>
2016-01-22 12:02:06 +0100

    more complex directive (current weather in Warsaw)

--- app/js/modules/main/directives.js
+++ app/js/modules/main/directives.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app.main.directives', [])
+angular.module('app.main.directives', ['restModule'])
 
 /**
  * current-time
@@ -11,4 +11,21 @@ angular.module('app.main.directives', [])
 		replace: true, // generated template will replace the HTML element on which the directive is applied
 		template: '<h5>'+new Date()+'</h5>'
 	};
-});
+})
+
+/**
+ * current-
+ */
+.directive('currentWeatherInWarsaw', ['WeatherService', function(WeatherService){
+	return {
+		restrict: 'AEC', // directive can be used as an attribute, a HTML element or class.
+		replace: true, // generated template will replace the HTML element on which the directive is applied
+		templateUrl: 'views/weather-info.html',
+		controller: function ($scope, $element, $attrs, $transclude) {
+			WeatherService.currentWeather('Warsaw')
+		    .success(function(data,status,config,headers){
+		    	$scope.weather = data;
+    		});			
+		}
+	};
+}]);
