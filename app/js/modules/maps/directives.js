ca5ce0b40921568e35447432ef6cc72b7a6e074e
marioosh <marioosh.net@gmail.com>
2016-01-27 08:44:19 +0100

    google maps directive - 2 versions

--- app/js/modules/maps/directives.js
+++ app/js/modules/maps/directives.js
@@ -4,7 +4,7 @@ angular.module('app.maps.directives', [])
 
 .directive('googleMaps', ['GOOGLE_MAPS_API_KEY', 'GOOGLE_MAPS_API_URL', '$sce', function(GOOGLE_MAPS_API_KEY, GOOGLE_MAPS_API_URL, $sce){
 	return {
-		scope: {
+		scope: { // isolated scope
     		height: '@height', // @ or @attr - bind a local scope property to the value of DOM attribute.
         	width: '@width',
         	coord: '=' // = or =attr - set up bi-directional binding between a local scope property and the parent scope property of name defined via the value of the attr attribute
@@ -14,15 +14,40 @@ angular.module('app.maps.directives', [])
 		templateUrl: 'views/google-maps.html',
         link: function(scope, elem, attrs) {
         	scope.$watch('coord', function(value,old){
-                if(value!=old) {
-                    if(scope.coord!=null) {
-                        //elem.attr('src', GOOGLE_MAPS_API_URL+'/view?center='+scope.coord.lat+','+scope.coord.lon+'&zoom=10&key='+GOOGLE_MAPS_API_KEY);
-                        scope.src = $sce.trustAsResourceUrl(GOOGLE_MAPS_API_URL+'/view?center='+scope.coord.lat+','+scope.coord.lon+'&zoom=10&key='+GOOGLE_MAPS_API_KEY);;
-                    }
+                if(value!=null&&value!='') {
+                    //elem.attr('src', GOOGLE_MAPS_API_URL+'/view?center='+scope.coord.lat+','+scope.coord.lon+'&zoom=10&key='+GOOGLE_MAPS_API_KEY);
+                    scope.src = $sce.trustAsResourceUrl(GOOGLE_MAPS_API_URL+'/view?center='+scope.coord.lat+','+scope.coord.lon+'&zoom=10&key='+GOOGLE_MAPS_API_KEY);
                 }
             });
         }
 	};	
 }])
+
+.directive('googleMapsV2', ['GOOGLE_MAPS_API_KEY', 'GOOGLE_MAPS_API_URL', '$sce', function(GOOGLE_MAPS_API_KEY, GOOGLE_MAPS_API_URL, $sce){
+    return {
+        scope: { // isolated scope
+            height: '@height', // @ or @attr - bind a local scope property to the value of DOM attribute.
+            width: '@width',
+            lat: '@',
+            lon: '@'            
+        },
+        restrict: 'AEC', // directive can be used as an attribute, a HTML element or class.
+        replace: true, // generated template will replace the HTML element on which the directive is applied
+        templateUrl: 'views/google-maps.html',
+        link: function(scope, elem, attrs) {
+            scope.$watch('lat', function(value,old){
+                if(value!=null&&value!='') {
+                    scope.src = $sce.trustAsResourceUrl(GOOGLE_MAPS_API_URL+'/view?center='+scope.lat+','+scope.lon+'&zoom=10&key='+GOOGLE_MAPS_API_KEY);
+                }
+            });
+            scope.$watch('lon', function(value,old){
+                if(value!=null&&value!='') {
+                    scope.src = $sce.trustAsResourceUrl(GOOGLE_MAPS_API_URL+'/view?center='+scope.lat+','+scope.lon+'&zoom=10&key='+GOOGLE_MAPS_API_KEY);
+                }
+            });                        
+        }
+    };  
+}])
+
 .value('GOOGLE_MAPS_API_URL', 'https://www.google.com/maps/embed/v1')
 .value('GOOGLE_MAPS_API_KEY', 'AIzaSyC1WrNUv4iF_ay6EiohLYa34KnXz1mMIB8');
